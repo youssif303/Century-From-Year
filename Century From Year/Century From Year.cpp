6b805886c1bc3c98d1ff19694ec3e66633df71b1
youss <youss@YOUSSIF>
2024-10-18 11:47:56 +0200

    Add project files.

---
+++ Century From Year/Century From Year.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iostream>
+using namespace std;
+void century(int year)
+{
+	
+	if (year % 100 == 0)
+		cout << "we are in Century= " << year/100;
+	else
+		cout << "we are in Century= " << year / 100 +1;
+}    
+int main()
+{
+	century(2024);
+	//ceil(1805/100);
+	return 0;
+}
